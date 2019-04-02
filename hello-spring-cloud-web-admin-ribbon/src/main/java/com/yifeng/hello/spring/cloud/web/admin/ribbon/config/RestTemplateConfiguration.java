c84616dcb1291d58bdef87572190985c18856e79
yifeng <9282207@qq.com>
2019-04-02 17:52:10 +0800

    服务消费者（Ribbon） Ribbon 是一个负载均衡客户端，可以很好的控制 http 和 tcp 的一些行为。

---
+++ hello-spring-cloud-web-admin-ribbon/src/main/java/com/yifeng/hello/spring/cloud/web/admin/ribbon/config/RestTemplateConfiguration.java
@@ -0,0 +1,15 @@
+package com.yifeng.hello.spring.cloud.web.admin.ribbon.config;
+
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class RestTemplateConfiguration {
+    @Bean
+    @LoadBalanced
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+}
---
+++ hello-spring-cloud-web-admin-ribbon/src/main/java/com/yifeng/hello/spring/cloud/web/admin/ribbon/service/AdminService.java
@@ -0,0 +1,22 @@
+package com.yifeng.hello.spring.cloud.web.admin.ribbon.service;
+
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class AdminService {
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @HystrixCommand(fallbackMethod = "hiError")
+    public String sayHi(String message) {
+        return restTemplate.getForObject("http://hello-spring-cloud-service-admin/hi?message=" + message, String.class);
+    }
+
+    public String hiError(String message) {
+        return "Hi，your message is :\"" + message + "\" but request error.";
+    }
+}
