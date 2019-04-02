82b8ab45ab8b0d6397d8305b2fbaf25e07cf7f98
yifeng <9282207@qq.com>
2019-04-02 17:49:39 +0800

    服务提供者

---
+++ hello-spring-cloud-service-admin/src/main/java/com/yifeng/hello/spring/cloud/service/admin/ServiceAdminApplication.java
@@ -0,0 +1,12 @@
+package com.yifeng.hello.spring.cloud.service.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceAdminApplication.class, args);
+    }
+}
---
+++ hello-spring-cloud-service-admin/src/main/java/com/yifeng/hello/spring/cloud/service/admin/controller/AdminController.java
@@ -0,0 +1,19 @@
+package com.yifeng.hello.spring.cloud.service.admin.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AdminController {
+
+    @Value("${server.port}")
+    private String port;
+
+    @RequestMapping(value = "hi", method = RequestMethod.GET)
+    public String sayHi(@RequestParam(value = "message") String message) {
+        return String.format("Hi，your message is : %s i am from port : %s", message, port);
+    }
+}
